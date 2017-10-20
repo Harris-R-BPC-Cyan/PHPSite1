9a1cf2885cf5c6aedbe833bb62b1dcc2c47a131f
Ryan Harris <32195073+Harris-R-BPC-Cyan@users.noreply.github.com>
2017-10-20 10:08:02 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,18 +6,11 @@
 
 echo "my first time trying PHP!";
 //this is me testing the site.
-$myfirst = "Hello world of course!";
-$FirstNum = 6;
-$firstFloat = 12.5;
 $this = "Hellow world!";
 $x = 2;
 $y = 2;
 
-echo $myfirst;
-echo "<br>";
-echo $firstNum;
-echo"<br>";
-echo $firstFloat;
+
 echo $txt;
 echo "<br>";
 echo $x;
