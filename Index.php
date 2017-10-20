1264addc9912b94943c2ae33e181f3698aa31afb
Ryan Harris <32195073+Harris-R-BPC-Cyan@users.noreply.github.com>
2017-10-20 10:00:04 +0100

    Testing variables

--- Index.php
+++ Index.php
@@ -5,8 +5,16 @@
 <?php
 
 echo "my first time trying PHP!";
-
 //this is me testing the site.
+$myfirst = "Hello world ofcourse!";
+$FirstNum = 6;
+$firstFloat = 12.5;
+
+echo $myfirst;
+echo "<br>";
+echo $firstNum;
+echo $firstFloat;
+ ?>
 
 ?>
 
